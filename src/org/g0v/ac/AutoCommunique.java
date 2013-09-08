076c758eb60abad3c3dc8f457c07274fec76c97f
Lee <jessy1092@gmail.com>
2013-09-09 01:47:08 +0800

    做好可以抓取公報資料 version 0.1

---
+++ src/org/g0v/ac/AutoCommunique.java
@@ -0,0 +1,110 @@
+package org.g0v.ac;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.oauth.client.OAuthClientFilter;
+import com.sun.jersey.oauth.signature.OAuthParameters;
+import com.sun.jersey.oauth.signature.OAuthSecrets;
+
+
+
+public class AutoCommunique 
+{
+	private String CLIENT_ID;
+	private String SECRET;
+	private String[] COMMUNIQUES_URI;
+	private static final String API_KEYS_FILE = "api_key.txt";
+	private static final String COMMUNIQUES_FILE = "communiques.txt";
+	
+	public AutoCommunique()
+	{
+		setApiKey();
+		setCommuniqueUri();
+	}
+	
+	public void run()
+	{
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		OAuthClientFilter filter = new OAuthClientFilter(
+				client.getProviders(), 
+				new OAuthParameters().consumerKey(CLIENT_ID), 
+				new OAuthSecrets().consumerSecret(SECRET));		
+		client.addFilter(filter);
+		for(int i = 0; i < COMMUNIQUES_URI.length; i++)
+		{
+			WebResource service = client.resource(
+					UriBuilder.fromUri(COMMUNIQUES_URI[i]).build());			
+			System.out.println(service.accept(MediaType.APPLICATION_JSON).get(String.class));
+		}
+		
+	}
+	
+	public void setApiKey()
+	{
+		try
+		{
+			FileReader fr = new FileReader(API_KEYS_FILE);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			if((line = br.readLine()) != null)
+			{
+				String[] para= line.split(" ");
+				CLIENT_ID = para[0];
+				SECRET = para[1];
+			}
+			br.close();
+			fr.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+	
+	public void setCommuniqueUri()
+	{
+		try
+		{
+			FileReader fr = new FileReader(COMMUNIQUES_FILE);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			String tmpCom = "";
+			String[] tmpuri;
+			while((line = br.readLine()) != null)
+			{
+				tmpCom += line + " ";
+			}
+			br.close();
+			fr.close();
+			tmpuri = tmpCom.split(" ");
+			COMMUNIQUES_URI = new String[tmpuri.length];
+			for(int i = 0; i < tmpuri.length; i++)
+			{
+				COMMUNIQUES_URI[i] = String.format("https://g0v.hackpad.com/api/1.0/pad/%s/content/latest.md", tmpuri[i]);
+			}
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		AutoCommunique autoCommunique = new AutoCommunique();		
+		autoCommunique.run();
+	}
+
+}
---
+++ src/org/g0v/ac/content/Content.java
@@ -0,0 +1,44 @@
+package org.g0v.ac.content;
+
+public class Content 
+{
+	private String content;
+	private String date;
+	private String tag;
+	
+	public Content()
+	{
+		
+	}
+	
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	
+	public String getContent()
+	{
+		return this.content;
+	}
+	
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	
+	public String getDate()
+	{
+		return this.date;
+	}
+	
+	public void setTag(String tag)
+	{
+		this.tag = tag;
+	}
+	
+	public String getTag()
+	{
+		return this.tag;
+	}
+
+}
